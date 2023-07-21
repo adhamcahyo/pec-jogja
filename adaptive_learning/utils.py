1b5d804b3b6abaab5af19ea0cecfcf5afdce42b2
adhamcahyo <nebelwerfersf@gmail.com>
2023-07-22 00:07:54 +0700

    v1

--- adaptive_learning/utils.py
+++ adaptive_learning/utils.py
@@ -1,1 +1,16 @@
- 
+from lms_app.models import Course
+
+def analyze_learning_style(user):
+    learning_style = user.learning_style
+
+    
+    if learning_style == 'visual':
+        adaptive_courses = Course.objects.filter(learning_style='visual')
+    elif learning_style == 'auditory':
+        adaptive_courses = Course.objects.filter(learning_style='auditory')
+    elif learning_style == 'kinesthetic':
+        adaptive_courses = Course.objects.filter(learning_style='kinesthetic')
+    else:
+        adaptive_courses = Course.objects.all()
+
+    return adaptive_courses
